0d82136449b41949492162769a6e722404532abf
TheodorPopescu <tedi.popescu05@gmail.com>
2026-03-19 18:46:54 +0200

    Web Build Changes

--- Assets/CloudLeaderboard.cs
+++ Assets/CloudLeaderboard.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using UnityEngine.Networking;
 using System.Collections;
 using System.Text;
-using UnityEditor.PackageManager.Requests;
+
 
 public class CloudLeaderboard : MonoBehaviour
 {
